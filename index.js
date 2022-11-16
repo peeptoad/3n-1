3eec669b7d0b7e6a91cb9000c94756a5e95afd7f
peeptoad <112097932+peeptoad@users.noreply.github.com>
2022-11-16 13:00:40 -0600

    Version1.2
    update for mobile

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 
 
 function generate() {
+    
+    
     let value = document.getElementById("input").value;
-
-    while (value > 1) {
-        if (value % 2 == 0) {
+    document.getElementById('log').innerHTML = '';
+    console.log(value)
+   
+    
+    
+    while (value > 1) 
+    
+       {
+        if (value % 2 == 0) 
+        {
             value = value / 2
             console.log(value)
         }
@@ -13,21 +22,22 @@ function generate() {
             value = value * 3 + 1
             console.log(value)
         }
-      
-
-    (function () {
-        var old = console.log;
-        var logger = document.getElementById('log');
-        console.log = function () {
-          for (var i = 0; i < arguments.length; i++) {
-            if (typeof arguments[i] == 'object') {
-                logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
-            } else {
-                logger.innerHTML += arguments[i] + '<br />';
+     
+        (function () {
+            var old = console.log;
+            var logger = document.getElementById('log');
+            console.log = function () {
+              for (var i = 0; i < arguments.length; i++) {
+                if (typeof arguments[i] == 'object') {
+                    logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 0) : arguments[i]) + '<br />';
+                } else {
+                    logger.innerHTML += arguments[i] + '<br />';
+                }
+              }
             }
-          }
-        }
-    })();
-
+        })();
+        
+       
+    
     }    
 }
