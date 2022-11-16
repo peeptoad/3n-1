bc5868d4edb44b7ac79fbaa83ddcf846afb29ef3
peeptoad <112097932+peeptoad@users.noreply.github.com>
2022-11-15 23:49:23 -0600

    Version1

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+
+ 
+
+    function generate() {
+    let value = document.getElementById("input").value;
+
+    while (value > 1) {
+        if (value % 2 == 0) {
+            value = value / 2
+            console.log(value)
+        }
+        else  {
+            value = value * 3 + 1
+            console.log(value)
+        }
+      
+
+    (function () {
+        var old = console.log;
+        var logger = document.getElementById('log');
+        console.log = function () {
+          for (var i = 0; i < arguments.length; i++) {
+            if (typeof arguments[i] == 'object') {
+                logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
+            } else {
+                logger.innerHTML += arguments[i] + '<br />';
+            }
+          }
+        }
+    })();
+
+    }    
+}
+
